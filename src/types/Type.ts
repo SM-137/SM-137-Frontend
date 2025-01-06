b2f31fc2a6db183b207a0ad3128bb471b9946f89
hanheel <hanhee630630@gmail.com>
2025-01-06 10:48:29 +0900

    fix : 변경된 mockData에 따라 type 변경

--- src/types/Type.ts
+++ src/types/Type.ts
@@ -1,15 +1,18 @@
+import { categoryName } from "../utils/SubCategoryContent";
+
 export type StatusType = "inProgress" | "pending" | "rejected" | "completed";
 
 export interface DataType {
   id: number;
   title: string;
   status: StatusType;
-  category: string[];
+  hashtag: string[];
   content: string;
   likes: number;
   bookmarks: number;
   date: string;
   answer: string;
+  category: string;
 }
 export interface CommentType {
   content: string;
@@ -36,3 +39,7 @@ export interface SortStandardProps {
   type: "latest" | "scrap" | "likes";
 }
 export type SortType = "latest" | "scrap" | "likes";
+
+export type CategoryValue =
+  | (typeof categoryName)[keyof typeof categoryName][number]
+  | undefined;
