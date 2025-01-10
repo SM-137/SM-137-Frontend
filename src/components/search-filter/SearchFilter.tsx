4738c1622138a5724334f96d994bf244ab5db4b9
hanheel <hanhee630630@gmail.com>
2025-01-10 09:49:36 +0900

    feat : 해시태그를 입력하고 등록하는 로직을 작성한다

--- src/components/search-filter/SearchFilter.tsx
+++ src/components/search-filter/SearchFilter.tsx
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled";
 import FilterListIcon from "@mui/icons-material/FilterList";
 import StatusButtonGroup from "../status-button/StatusButtonGroup"; // StatusButtonGroup 경로에 맞게 수정
+import { useState } from "react";
+import CategoryTag from "../category-tag/CategoryTag";
 
 const Container = styled.div`
   width: 100%;
@@ -14,6 +16,12 @@ const Container = styled.div`
     flex-direction: column;
     gap: 1rem;
   }
+`;
+
+//해시태그 input + 검색 결과
+const HashtagArea = styled.div`
+  display: flex;
+  gap: 1rem;
 `;
 
 const SearchBar = styled.div`
@@ -41,10 +49,13 @@ const SearchInput = styled.input`
   &::placeholder {
     color: var(--gray4-placeholder-low);
   }
+  &:focus::placeholder {
+    opacity: 0;
+  }
 `;
 
 const FilterIcon = styled(FilterListIcon)`
-  color: gray5-lowText;
+  color: var(--gray5-lowText);
   cursor: pointer;
 `;
 
@@ -52,15 +63,60 @@ const StatusButtonGroupContainer = styled.div`
   display: flex;
 `;
 
+const HashtagContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`;
+
 const SearchFilterBar = () => {
+  const [hashtagArray, setHashtagArray] = useState<string[]>([]);
+  const [hashtagInput, setHashtagInput] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHashtagInput(e.target.value);
+  };
+
+  const handleHashtagEnter = () => {
+    validateHashtag();
+    setHashtagArray((prev) => [...prev, hashtagInput]);
+    setHashtagInput("");
+  };
+
+  const validateHashtag = () => {
+    if (hashtagInput.length === 0 || !hashtagInput) {
+      return;
+    }
+    if (hashtagArray.includes(hashtagInput)) {
+      return;
+    }
+  };
+
+  const handleHashtagDelete = (value: string) => {
+    setHashtagArray((prev) => prev.filter((i) => i !== value));
+  };
+  const isHashtagSearchActive = hashtagArray.length !== 0;
+
   return (
     <Container>
       {/* 왼쪽 서치바 */}
-      <SearchBar>
-        <SearchText>해시태그</SearchText>
-        <SearchInput placeholder="졸업" />
-        <FilterIcon />
-      </SearchBar>
+      <HashtagArea>
+        <SearchBar>
+          <SearchText>해시태그</SearchText>
+          <SearchInput placeholder="졸업" onChange={handleChange} />
+          <FilterIcon onClick={handleHashtagEnter} />
+        </SearchBar>
+
+        {isHashtagSearchActive && (
+          <HashtagContainer>
+            {hashtagArray.map((i) => (
+              <CategoryTag
+                contents={i}
+                // onClick={() => handleHashtagDelete(i)}
+              />
+            ))}
+          </HashtagContainer>
+        )}
+      </HashtagArea>
 
       {/* 오른쪽 상태 버튼 그룹 */}
       <StatusButtonGroupContainer>
