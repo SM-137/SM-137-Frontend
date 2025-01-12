5ff615d49621ec9c637fb590ba141b6db9524a33
hanheel <hanhee630630@gmail.com>
2025-01-12 18:17:43 +0900

    fix : 배포용 baseUrl을 변경한다

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "SM-137-Frontend",
+  base: "/SM-137-Frontend/",
   server: {
     proxy: {
       "/api": {
