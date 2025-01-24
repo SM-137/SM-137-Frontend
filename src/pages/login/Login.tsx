b668bc170af5df89561a3b0fac04ac430b802fce
hanheel <hanhee630630@gmail.com>
2025-01-25 02:33:43 +0900

    fix : 이미지 경로에서 basename을 삭제한다

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -86,7 +86,7 @@ const LoginButton = styled(GoogleLoginSvg)`
 `;
 
 const Login = () => {
-  const IMAGE_SRC = "/src/assets/emblem-1_DarkGray.png";
+  const IMAGE_SRC = "/assets/emblem-1_DarkGray.png";
 
   return (
     <Background>
