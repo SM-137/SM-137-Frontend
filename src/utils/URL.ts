ef1a56f616e5a3d926aa3f462161f3740bcf7239
hanheel <hanhee630630@gmail.com>
2025-01-25 01:35:56 +0900

    feat : basename 및 baseurl을 변경한다

--- src/utils/URL.ts
+++ src/utils/URL.ts
@@ -1,6 +1,3 @@
-export const BASE_NAME = "/SM-137-Frontend";
-export const BASE_URL = "https://sm-137.github.io/SM-137-Frontend/";
-
 export const VIEW_URL = "/complaint-view";
 export const HOME_URL = "/";
 export const MYPAGE_URL = "/mypage";
