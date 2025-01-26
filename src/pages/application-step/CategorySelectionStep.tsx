0e98875ee252e31de52cf20b9d24b7a8e3ad9773
Chaejy <xhdrp@sookmyung.ac.kr>
2025-01-26 14:13:43 +0900

    feat, design: 신청 2단계 디자인 수정 및 페이지 이동

--- src/pages/application-step/CategorySelectionStep.tsx
+++ src/pages/application-step/CategorySelectionStep.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { useNavigate } from "react-router-dom";
 import Layout from "./ApplicationStepLayout";
 import CategorySelect from "../../components/category-select/CategorySelect";
 import Button from "../../components/button/Button";
@@ -6,7 +7,7 @@ import Button from "../../components/button/Button";
 const ContentWrapper = styled.div`
   background-color: var(--white);
   border-radius: 8px;
-  padding: 2rem;
+  padding: 1.5rem;
   width: 100%;
   margin: 3rem auto 0;
   display: flex;
@@ -35,20 +36,30 @@ const ButtonGroup = styled.div`
 `;
 
 const CategorySelectionStep = () => {
+  const navigate = useNavigate();  
+
+  const handleBack = () => {
+    navigate("../1", { replace: true });
+  };
+
+  const handleNext = () => {
+    navigate("../3", { replace: true });
+  };
+
   const handleCategoryChange = (selectedCategory: string) => {
     console.log(`Selected Category: ${selectedCategory}`);
   };
 
   return (
-    <Layout>
+    <Layout activeStep={2}>
       <ContentWrapper>
         <FormTitleContainer>
           <FormTitle>가장 연관이 깊은 1개의 카테고리(분류)를 선택해 주세요</FormTitle>
         </FormTitleContainer>
         <CategorySelect usage="normal" onCategoryChange={handleCategoryChange} />
         <ButtonGroup>
-          <Button content="이전" type="_120x40_Gray2" />
-          <Button content="다음" type="_120x40_Primary" />
+          <Button content="이전" type="_120x40_Gray2" onClick={handleBack} />
+          <Button content="다음" type="_120x40_Primary" onClick={handleNext} />
         </ButtonGroup>
       </ContentWrapper>
     </Layout>
